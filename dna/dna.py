f898b6a9f6d358b44d481ab9a9d2181fc83b357d
Jarnster <jarneverlinden4@gmail.com>
2025-03-31 20:30:01 +0200

    Update dna.py

--- dna/dna.py
+++ dna/dna.py
@@ -1,30 +1,27 @@
 import csv
 import sys
 
-
 def main():
-    # Controleer het aantal argumenten
+    # Check arg length
     if len(sys.argv) != 3:
         print("Usage: python dna.py <database.csv> <dna_sequence.txt>")
         sys.exit(1)
 
-    # Haal de bestandsnamen uit de argumenten
     db_filename = sys.argv[1]
     dna_filename = sys.argv[2]
 
-    # Laad de database
     db = load_database(db_filename)
 
-    # Laad de DNA-sequentie
+    # Sequences #
     sequence = load_dna_sequence(dna_filename)
 
-    # Bereken de STR-aantallen in de DNA-sequentie
+    # STR-counting
     str_counts = {}
     for str_name in db[0].keys():
         if str_name != 'name':
             str_counts[str_name] = longest_match(sequence, str_name)
 
-    # Vergelijk de STR-aantallen met de database
+    # STR-Compare
     match = check_for_match(db, str_counts)
     
     if match:
@@ -34,7 +31,6 @@ def main():
 
 
 def load_database(filename):
-    """Leest het CSV-bestand en retourneert de gegevens als een lijst van dictionaries."""
     db = []
     with open(filename, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
@@ -44,13 +40,11 @@ def load_database(filename):
 
 
 def load_dna_sequence(filename):
-    """Leest het DNA-bestand en retourneert de inhoud als een string."""
     with open(filename, "r") as f:
         return f.read().strip()
 
 
 def longest_match(sequence, subsequence):
-    """Retourneert de lengte van het langste aantal aaneengeschakelde herhalingen van subsequence in sequence."""
     longest_run = 0
     subsequence_length = len(subsequence)
     sequence_length = len(sequence)
@@ -70,7 +64,6 @@ def longest_match(sequence, subsequence):
 
 
 def check_for_match(db, str_counts):
-    """Vergelijkt de STR-aantallen met elk profiel in de database."""
     for person in db:
         match = True
         for str_name, count in str_counts.items():
